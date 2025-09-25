12f4ef315ae212b5d6f4f6c04d439b48c15c9ed7
Raju_0005 <rajugbp2006@gmail.com>
2025-09-25 17:32:34 +0530

    Update db.js

--- server/_helpers/db.js
+++ server/_helpers/db.js
@@ -12,15 +12,15 @@ async function initialize() {
   // const { port, user, password, database } = config.database;
 
   // Create the database if it doesn't exist
-  const connection = await mysql.createConnection({
-    host,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  });
-  await connection.query(
-    `CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`
-  );
+  // const connection = await mysql.createConnection({
+  //   host,
+  //   port: process.env.DB_PORT,
+  //   user: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  // });
+  // await connection.query(
+  //   `CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`
+  // );
 
   // Connect to database
   const sequelize = new Sequelize(
